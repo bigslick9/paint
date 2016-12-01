471d78f7997cc026f82b46c0fd776099bc5d4f2e
student <student@240-07.INFORMATYKA.local>
2016-12-01 12:58:13 +0200

    lista figur(kolo + kwadrat)

---
+++ src/paint/Kwadrat.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package paint;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+/**
+ *
+ * @author student
+ */
+public class Kwadrat implements Figura {
+   private int x=-1;
+    private int y=-1;
+    public Kwadrat(int a,int b){
+        this.x=a;
+        this.y=b;
+    }
+    
+    public void paint(Graphics g)
+ {
+ g.setColor(Color.RED);
+ if(x!=-1 && y!=-1)
+ g.drawRect(x-25, y-25, 50, 50);
+ }
+
+} 
+
